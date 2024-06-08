c84b15f7dc548c0e29520796fa09c265cb52b13f
Atabong Cecilia <catabong89@gmail.com>
2024-06-08 17:42:41 +0100

    updated settings.py

--- nenesDelicacy/settings.py
+++ nenesDelicacy/settings.py
@@ -155,7 +155,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATICFILES_DIRS = str(BASE_DIR/'staticfiles')
+STATICFILES_DIRS = [str(BASE_DIR/'staticfiles')]
 STATIC_ROOT = str(BASE_DIR/'static')
 STATIC_URL = '/static/'
 
