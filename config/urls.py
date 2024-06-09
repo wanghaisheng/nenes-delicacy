9e2ea6b62e5c64ca006c742112a14c44fff41728
Atabong Cecilia <catabong89@gmail.com>
2024-06-09 19:51:03 +0100

    enclosed MEDIA ROOT in str()

--- config/urls.py
+++ config/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('payment', views.payment, name='payment')
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
