f8f3b282f24f58911497834ba80a587540c0c517
Atabong Cecilia <catabong89@gmail.com>
2024-06-09 20:04:30 +0100

    minor change

--- config/urls.py
+++ config/urls.py
@@ -22,5 +22,4 @@ urlpatterns = [
     path('payment', views.payment, name='payment')
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
