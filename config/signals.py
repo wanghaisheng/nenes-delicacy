ddb064c0d200d2e6791a87ce2a8b210cfd965860
Atabong Cecilia <catabong89@gmail.com>
2024-06-17 00:49:58 +0100

    implemented code to create placeholder

--- config/signals.py
+++ config/signals.py
@@ -1,20 +1,29 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from PIL import Image
+import cloudinary.uploader
+from PIL import Image, ImageFilter
+from io import BytesIO
 from .models import Products
+import requests
+import os
 
 @receiver(post_save, sender=Products)
 def create_placeholder(sender, instance, **kwargs):
-    print('called')
-    if instance.image:  
-        original_image = Image.open(instance.image)
 
-        if original_image.mode != 'RGB':
-            original_image = original_image.convert('RGB')
+    response = requests.get(instance.image.url)
+    file_path = f'media/{instance.name}_placeholder.jpg'
+    if os.path.exists(file_path):
+        return
+     
+    if response.status_code == 200:
+        image = Image.open(BytesIO(response.content)).convert('RGB')
+
+        image = image.resize((200, 200), Image.ANTIALIAS).filter(ImageFilter.BLUR)
 
         # Save placeholder image
-        original_image.save(f'media/{instance.name}_placeholder', 'JPEG', quality=5)
+        image.save(file_path)
+        placeholder = cloudinary.uploader.upload(file_path)
 
-        # Update model instance if necessary
-        instance.lazyImage = f'media/{instance.name}_placeholder.jpg'
-        instance.save()
+        # Update model instance
+        instance.lazyImage = placeholder['secure_url']
+        instance.save() 
