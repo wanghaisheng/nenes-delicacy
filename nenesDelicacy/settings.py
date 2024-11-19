ff648ad894c6499ef01fe01b3cf522abad5a1611
Atabong Cecilia <catabong89@gmail.com>
2024-11-19 18:25:05 +0100

    switched to cloud redis

--- nenesDelicacy/settings.py
+++ nenesDelicacy/settings.py
@@ -142,26 +142,26 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# CACHES = {
-#     "default": {
-#         "BACKEND": "django_redis.cache.RedisCache",
-#         "LOCATION": env('REDIS_URL'),
-#         "OPTIONS": {
-#             "CLIENT_CLASS": "django_redis.client.DefaultClient",
-#         }
-#     }
-# }
- 
-
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": env('REDIS_URL'),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
     }
 }
+ 
+
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django_redis.cache.RedisCache",
+#         "LOCATION": "redis://127.0.0.1:6379/1",
+#         "OPTIONS": {
+#             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+#         }
+#     }
+# }
 
 
 SESSION_ENGINE = "django.contrib.sessions.backends.cache"
